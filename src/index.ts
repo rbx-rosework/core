6f2749bdb467b8a527a79c080b8289dbef7e21bb
TypeChecked <roseau.business@gmail.com>
2022-04-15 11:32:37 -0400

    Index - Export sub entity

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@
 
 // Imports
 import { Entity, EntityMetadata } from "./Entity"
+import { SubEntity, SubEntityType, SubEntityInfo } from "./Entity/SubEntity"
 import { RoseworkManager } from "./RoseworkManager"
 
 // Exports
 export { Entity, EntityMetadata }
+
+export { SubEntity, SubEntityType, SubEntityInfo }
 
 export { RoseworkManager }
